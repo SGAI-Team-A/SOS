ea845d94a189c36ca45b3b636f8e5b8859f6a62b
Sabrina <syenko@mit.edu>
2023-07-20 14:46:40 -0700

    Add Env starter code

--- rl_training/game_environment.py
+++ rl_training/game_environment.py
@@ -1,1 +1,15 @@
-# Build Env class here
+import gymnasium as gym
+import numpy as np
+
+class GameEnv(gym.Env):
+    def __init__(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def render(self):
+        pass
+
+    def reset(self):
+        pass
