acc611c74c1b98055e6d0d50417c1c4ab31c2f72
Sabrina <syenko@mit.edu>
2023-07-20 14:48:19 -0700

    Add base training code

--- rl_training/training.py
+++ rl_training/training.py
@@ -1,1 +1,15 @@
-# Train here
+import gymnasium
+from game_environment import GameEnv
+
+env = GameEnv()
+observation, info = env.reset()
+
+for _ in range(1000):
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+
+env.close()
