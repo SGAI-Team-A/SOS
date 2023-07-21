7a42346c3c0350badad246877abe949bbe419095
Sabrina <syenko@mit.edu>
2023-07-20 22:46:43 -0700

    Update scorekeeper to account for rewards, add to env step function
    Created increment_scorekeeper in the ScoreKeeper class (replaces direct calls to __scorekeeper) in order to account for rewards in one place

--- rl_training/game_environment.py
+++ rl_training/game_environment.py
@@ -7,7 +7,6 @@ from gymnasium import spaces
 from endpoints.data_parser import DataParser
 from gameplay.scorekeeper import ScoreKeeper
 
-
 class GameEnv(gym.Env):
     def __init__(self, data_parser: DataParser):
         self.data_parser = data_parser
@@ -25,6 +24,17 @@ class GameEnv(gym.Env):
         # 4 actions: save, scram, skip, squish
         self.action_space = spaces.Discrete(4)
 
+    # perform relevant function based on action number
+    def _action_to_function(self, action):
+        if action == 0:
+            self.scorekeeper.save(self.humanoid)
+        elif action == 1:
+            self.scorekeeper.scram()
+        elif action == 2:
+            self.scorekeeper.skip(self.humanoid)
+        elif action == 3:
+            self.scorekeeper.squish(self.humanoid)
+
     def _get_obs(self):
         return {
             "humanoid_status": self.humanoid.get_state(),
@@ -36,7 +46,14 @@ class GameEnv(gym.Env):
         return None  # No extra info (maybe change later?
 
     def step(self, action):
-        pass
+        self._action_to_function(action)
+
+        reward = self.scorekeeper.get_reward()
+
+        observation = self._get_obs()
+        info = self._get_info()
+
+        # return observation, reward, terminated, truncated, info
 
     def render(self):
         pass
