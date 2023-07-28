e4928a6db93079985bd9abe1260de50d17a92c94
Alex <alexrw@mit.edu>
2023-07-28 16:32:37 -0500

    refactored buttons to exist in hud.py
    title

--- ui_elements/hud.py
+++ ui_elements/hud.py
@@ -8,6 +8,8 @@ from ui_elements.clock import Clock
 from ui_elements.status_card import StatusCard
 from ui_elements.capacity_meter import CapacityMeter
 from ui_elements.cures import CureCounter
+from ui_elements.button import Button
+from ui_elements.button_menu import ButtonMenu
 
 class HUD(object):
     def __init__(self, ui, root, w, h):
@@ -15,6 +17,7 @@ class HUD(object):
         self.canvas = root
         self.path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'graphics', 'post-render.png')
         self.ambulance = ImageTk.PhotoImage(Image.open(self.path).resize((w, h), Image.LANCZOS))
+        scale_factor = w / 1920
 
         self.hud_img = root.create_image(0, 0, anchor=tk.NW, image=self.ambulance, tags='hud')
 
@@ -26,6 +29,66 @@ class HUD(object):
 
         self.clock = Clock(self.canvas, w, h, self.ui.scorekeeper)
         self.meter = CapacityMeter(self.canvas, w, h, self.ui.data_parser.capacity)
+
+        # set up the buttons
+        def on_disabled():
+            self.game_viewer.hud.update_log.set_update("Not enough time left!"),
+
+        self.buttons = {
+            'skip': Button(
+                corners=[(1284, 675), (1550, 683), (1540, 832), (1280, 820) ],
+                on_click=lambda: [self.ui.scorekeeper.skip(self.ui.humanoid),
+                                  self.ui.update_ui(),
+                                  self.ui.get_next()],
+                on_disabled_click=on_disabled,
+                scale_factor=scale_factor
+            ),
+            'squish': Button(
+                corners=[(1280, 825), (1542, 840), (1540, 996), (1275, 972),],
+                on_click=lambda: [self.ui.scorekeeper.squish(self.ui.humanoid),
+                                  self.ui.update_ui(),
+                                  self.ui.get_next()],
+                on_disabled_click=on_disabled,
+                scale_factor=scale_factor
+            ),
+            'save': Button(
+                corners=[(1554, 682), (1850, 687), (1846, 852), (1550, 834)],
+                on_click=lambda: [self.ui.scorekeeper.save(self.ui.humanoid),
+                                  self.ui.update_ui(),
+                                  self.ui.get_next()],
+                on_disabled_click=on_disabled,
+                scale_factor=scale_factor
+            ),
+            'scram': Button(
+                corners=[(1550, 840), (1847, 856), (1845, 1022), (1548, 996)],
+                on_click=lambda: [self.ui.scorekeeper.scram(),
+                                  self.ui.update_ui(),
+                                  self.ui.get_next()],
+                on_disabled_click=on_disabled,
+                scale_factor=scale_factor
+            ),
+        }
+
+        # on hover - change cursor to click arrow
+        def on_move_callback(e):
+            # buttons aren't showing
+            if not any([button.is_on_game_screen() for button in self.buttons.values()]):
+                return
+            # normal buttons
+            elif any([button.is_touching(e.x, e.y) and not button.is_disabled() for button in self.buttons.values()]):
+                self.ui.set_cursor("hand2")
+            # buttons are disabled
+            elif any([button.is_touching(e.x, e.y) and button.is_disabled() for button in self.buttons.values()]):
+                self.ui.set_cursor("X_cursor")
+            # not touching buttons
+            elif not any([button.is_touching(e.x, e.y) for button in self.buttons.values()]):
+                self.ui.set_cursor("arrow")
+        self.ui.root.bind("<Motion>", on_move_callback, add="+")
+
+        self.button_menu = ButtonMenu(self.buttons)
+        self.button_menu.set_interactive(False)
+
+        self.ui.root.bind("<Button-1>", lambda e: self.ui.intro_cards.show_next())
 
         self.update_log = UpdateLog(self.canvas)
 
@@ -43,4 +106,11 @@ class HUD(object):
         self.meter.update_fill(self.ui.scorekeeper.get_current_capacity(), self.ui.scorekeeper.get_last_saved())
         self.cure_counter.update_text()
 
+    # nuke info card on click and make buttons interaction
+    def nuke(self, event=None):
+        self.ui.root.unbind("<Button-1>")
+
+        # bind button on click callback
+        self.ui.root.bind("<Button-1>", lambda e: [button.on_click_callback(e) for button in self.buttons.values()], add="+")
+        self.button_menu.set_interactive(True)
 
